edcca8f2af8923f56e72af73de8cd75843c7e0d8
Tom Conneely <tom.conneely@outlook.com>
2019-06-24 16:48:29 +0100

    Added Write-Log function

--- Modules/CommonTasks.psm1
+++ Modules/CommonTasks.psm1
@@ -50,5 +50,36 @@ function Confirm-ModuleInstalled {
     
 }
 
+<#
+    Found https://stackoverflow.com/questions/7834656/create-log-file-in-powershell
+#>
+Function Write-Log {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $False)]
+        [ValidateSet("INFO", "WARN", "ERROR", "FATAL", "DEBUG")]
+        [String]
+        $Level = "INFO",
+
+        [Parameter(Mandatory = $True)]
+        [string]
+        $Message,
+
+        [Parameter(Mandatory = $False)]
+        [string]
+        $logfile
+    )
+
+    $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+    $Line = "$Stamp $Level $Message"
+    If ($logfile) {
+        Add-Content $logfile -Value $Line
+    }
+    Else {
+        Write-Output $Line
+    }
+}
+
 Export-ModuleMember -Function Write-HostTextWithNewLine
 Export-ModuleMember -Function Confirm-ModuleInstalled
+Export-ModuleMember -Function Write-Log
