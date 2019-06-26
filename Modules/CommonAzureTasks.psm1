8b9de20fb918074c9128043e163d33985e3c2dfa
Tom Conneely <tom.conneely@outlook.com>
2019-06-26 18:14:17 +0100

    Added comments

--- Modules/CommonAzureTasks.psm1
+++ Modules/CommonAzureTasks.psm1
@@ -1,3 +1,26 @@
+<#
+    .SYNOPSIS
+        Activate an Azure subscription using its name
+
+    .DESCRIPTION
+        Activate an Azure subscription using the subscription name provided by the user.
+        If the subscription is already active then the script continues
+
+    .PARAMETER SubscriptionName
+        Name of the subscription 
+        
+    .INPUTS
+        None. You cannot pipe objects to Set-SubscriptionIfNotActive
+
+    .OUTPUTS
+        None. Set-SubscriptionIfNotActive does not generate any output.
+
+    .EXAMPLE
+        Set-SubscriptionIfNotActive "Subscription Name"
+        
+    .EXAMPLE
+        Set-SubscriptionIfNotActive -SubscriptionName "Subscription Name"
+#>
 Function Set-SubscriptionIfNotActive {
     param (
         [Parameter(Mandatory = $True, HelpMessage =
